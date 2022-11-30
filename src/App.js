32c28d2890e208eee46ea3f81bb9b62b9d3b05d8
swathigopavarapu <swathigopavarapu1@gmail.com>
2022-11-30 19:38:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {  useState } from 'react';
+import ReactEcharts from "echarts-for-react";
+import { Rnd } from "react-rnd";
 
 function App() {
+  const [state,setState] = useState({width:350,height:300})
+
+
+const style = {
+
+  border: "solid 1px #ddd",
+  top:'50px'
+
+};
+
+
+
+  const option = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: 'bar'
+      }
+    ]
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Noesys Assignment</h1>
+   <Rnd
+    style={style}
+    default={{
+      x: 0,
+      y: 0,
+      width: 350,
+      height: 300
+    }}
+  >  
+         <ReactEcharts option={option} />
+         </Rnd>
     </div>
   );
 }
